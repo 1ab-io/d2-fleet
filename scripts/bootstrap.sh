b93603281a7ff417e06b06e0d069e57e414cc8bc
Guillaume <4112243+LEI@users.noreply.github.com>
2025-08-09 22:54:32 +0200

    fix: e2e and hostname

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,14 +2,36 @@
 
 set -eux
 
+retry() {
+  max=5
+  retry=0
+  while ! "$@"; do
+    retry=$((retry + 1))
+    if [ $retry -ge $max ]; then
+      echo "Failed to apply manifests after $max attempts"
+      exit 1
+    fi
+    echo "Attempt $retry/$max, waiting 60 seconds..."
+    sleep 60
+  done
+}
+
 BRANCH="${BRANCH:-main}"
 BASE_URL="https://raw.githubusercontent.com/1ab-io/d2-infra/refs/heads/$BRANCH/manifests"
 
-# sh -c 'set -eux; BRANCH=main; apply() { for name in cert-manager cilium cert-manager.config flux-operator; do kubectl apply --filename="https://raw.githubusercontent.com/1ab-io/d2-infra/refs/heads/${BRANCH}/manifests/${name}.yaml" || return $?; done }; retry=0; while ! apply; do retry=$((retry + 1)); if [ $retry -ge 5 ]; then echo "Failed to apply manifests after 5 attempts"; exit 1; fi; sleep 10; done'
-# --force-conflicts=true --server-side=true --wait=false
+# kubectl cluster-info
+current_context="$(kubectl config current-context)"
+if [ "$current_context" != kind-kind ]; then
+  echo >&2 "Invalid context: $current_context"
+  exit 1
+fi
 
-# Ensure CRDs are installed first
-kubectl apply --filename="$BASE_URL/cert-manager.yaml"
+kubectl apply \
+  --filename=https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.2.0/experimental-install.yaml \
+  --filename=https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/refs/heads/main/example/prometheus-operator-crd/monitoring.coreos.com_servicemonitors.yaml
+
+kubectl apply --filename="$BASE_URL/cert-manager.yaml" || true
+# --force-conflicts=true --server-side=true --wait=false
 
 curl -LSfs "$BASE_URL/cilium.yaml" |
   sed -e 's/"localhost"/"kind-control-plane"/g' -e 's/"7445"/"6443"/g' \
@@ -22,23 +44,32 @@ curl -LSfs "$BASE_URL/cilium.yaml" |
 # kubectl --namespace=cert-manager wait pod -l app=cert-manager,webhook --for=condition=ready --timeout=5m
 # kubectl apply --filename=/tmp/cilium.yaml
 
-max=5 retry=0
-while ! kubectl apply --filename=/tmp/cilium.yaml; do
-  retry=$((retry + 1))
-  if [ $retry -ge $max ]; then
-    echo "Failed to apply manifests after $max attempts"
-    exit 1
-  fi
-  echo "Attempt $retry/$max, waiting 60 seconds..."
-  sleep 60
-done
+retry kubectl apply --filename=/tmp/cilium.yaml
 # --filename=$BASE_URL/flux-operator.yaml
 # --filename=clusters/$ENVIRONMENT/flux-system/flux-instance.yaml"
 
 # kubectl apply --filename=$BASE_URL/cilium.config.yaml
+HOSTNAME_CP1=kind-control-plane
+cat >/tmp/cilium-lb-ip-pool-$HOSTNAME_CP1.yaml <<EOF
+---
+apiVersion: cilium.io/v2alpha1
+kind: CiliumLoadBalancerIPPool
+metadata:
+  name: $HOSTNAME_CP1
+spec:
+  blocks:
+    - cidr: "$IPV4_ADDRESS/32"
+    # - cidr: "::/128"
+  disabled: false
+  serviceSelector:
+    matchLabels:
+      app.kubernetes.io/name: ingress-nginx
+      node: $HOSTNAME_CP1
+EOF
+kubectl apply --filename=/tmp/cilium-lb-ip-pool-$HOSTNAME_CP1.yaml
 
 kubectl apply --filename="$BASE_URL/cert-manager.config.yaml"
-# sh -c 'max=5 retry=0; while ! kubectl apply --filename=$BASE_URL/cert-manager.config.yaml && ; do retry=$((retry + 1)); if [ $retry -ge $max ]; then echo "Failed to apply manifests after $max attempts"; exit 1; fi; echo "Attempt $retry/$max, waiting 60 seconds..."; sleep 60; done'
+# retry kubectl apply --filename=$BASE_URL/cert-manager.config.yaml
 
 # helm uninstall flux-operator --namespace flux-system || true
 if ! helm status --namespace=flux-system flux-operator; then
@@ -74,30 +105,19 @@ kubectl apply --filename="clusters/$ENVIRONMENT/flux-system/flux-instance.yaml"
 
 kubectl --namespace=flux-system wait fluxinstance/flux --for=condition=ready --timeout=5m
 
-for name in gitlab-agent gitlab-runner; do
-  if kubectl --namespace=$name get kustomization/$name >/dev/null; then
-    kubectl --namespace=$name patch kustomization/$name \
-      --type=merge \
-      --patch='{"metadata":{"annotations":{"fluxcd.controlplane.io/reconcile":"false"}}}'
-  fi
-  if kubectl --namespace=$name get helmrelease/$name >/dev/null; then
-    kubectl --namespace=$name delete helmrelease/$name
-  fi
-done
-
 # kubectl --namespace=kube-system wait kustomization/cilium-controllers --for=create --timeout=5m
 # kubectl --namespace=kube-system patch kustomization/cilium-controllers \
 #   --type=merge \
 #   --patch='{"spec":{"suspend":true}}'
-#
-# kubectl --namespace=kube-system wait helmrelease/cilium --for=create --timeout=5m
-# # kubectl --namespace=kube-system annotate kustomization/cilium-controllers \
-# #   ustomize.toolkit.fluxcd.io/reconcile"="disabled" \
-# #   overwrite
+
+kubectl --namespace=kube-system wait helmrelease/cilium --for=create --timeout=5m
+# kubectl --namespace=kube-system annotate kustomization/cilium-controllers \
+#   ustomize.toolkit.fluxcd.io/reconcile"="disabled" \
+#   overwrite
 # kubectl --namespace=kube-system patch helmrelease/cilium \
 #   --type=merge \
 #   --patch='{"spec":{"values":{"k8sServiceHost":"kind-control-plane","k8sServicePort":6443}}}'
-#
+
 # # Remove Talos CCM
 # kubectl --namespace=kube-system wait helmrelease/talos-ccm --for=create --timeout=5m
 # kubectl --namespace=kube-system delete helmrelease/talos-ccm ocirepository/talos-ccm ocirepository/talos-cloud-controller-manager-chart kustomization/talos-ccm-controllers --wait=false
@@ -106,7 +126,10 @@ done
 # kubectl wait namespace/monitoring --for=create --timeout=5m
 # kubectl --namespace=monitoring wait kustomization/monitoring-controllers --for=condition=ready --timeout=5m
 # kubectl --namespace=monitoring wait kustomization/monitoring-configs --for=condition=ready --timeout=5m
-#
+
+kubectl wait namespace/ingress-nginx --for=create --timeout=5m
+kubectl --namespace=ingress-nginx wait kustomization/ingress-nginx-controllers --for=condition=ready --timeout=5m
+
 # kubectl wait namespace/backend --for=create --timeout=5m
 # kubectl --namespace=backend wait kustomization/apps --for=condition=ready --timeout=5m
 #
